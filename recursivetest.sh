b0d3fa46fbb134db9b8230b9c5c6d36302410804
Kenny Parsons <kennyparsons93@gmail.com>
2020-01-24 10:11:52 -0600

    Update recursivetest.sh

--- recursivetest.sh
+++ recursivetest.sh
@@ -5,4 +5,5 @@ timestamp=`date '+%Y.%m.%d.%H.%M.%S'`
 #find . -maxdepth 1 -mindepth 1 -type d -exec tar czf /root/test/{}.$timestamp.tar.gz {}  \;
 #find /root/docker -maxdepth 1 -mindepth 1 -type d -exec tar czvf /root/test/{}.$timestamp.tar.gz {}  \;
 cd /root/test
-find . -maxdepth 1 -mindepth 1 -type d -exec tar -czg /opt/backup/config/root/test/{} -f /gvault/backup/root/test/{}.$timestamp.tar.gz {}  \;
+find . -maxdepth 1 -mindepth 1 -type d -exec echo {}
+#find . -maxdepth 1 -mindepth 1 -type d -exec tar -czg /opt/backup/config/root/test/{} -f /gvault/backup/root/test/{}.$timestamp.tar.gz {}  \;
